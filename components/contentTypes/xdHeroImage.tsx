5ece0562d774577e7b76998c4b6cd9a777a38ba6
Derek Nerenberg <danerenberg@yahoo.com>
2022-01-11 05:48:14 -0600

    added animations to the top of the homescreen

--- components/contentTypes/xdHeroImage.tsx
+++ components/contentTypes/xdHeroImage.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import {initHomeAnimation} from "../animation/homeSplashScreen"
 import ContentfulImage from '../contentful-image';
 import {getBackgroundImageUrl} from '@lib/utilities';
 import XDLink from './xdLink';
@@ -14,6 +16,7 @@ const XDHeroImage = ({entry}: IXDHeroImageProps) => {
         )
     });
     const backgroundImage = getBackgroundImageUrl(entry.backgroundImage?.fields?.file?.url);
+    useEffect ( ()=> {initHomeAnimation(); }, [] );
     return (
         <div 
             className={cn('bg-gradient-to-b from-hero-bg-500 via-hero-bg-400 via-hero-bg-300 via-hero-bg-200 to-hero-bg-100 flex justify-center w-full max-h-[900px] -mt-20 pt-8 md:pt-16 lg:pt-24 relative z-10 hero-image', {
@@ -21,19 +24,19 @@ const XDHeroImage = ({entry}: IXDHeroImageProps) => {
             })}>
             <section className='text-gray-600 body-font'>
                 <div className='container mx-auto py-24 flex lg:flex-row flex-col items-center '>
-                    <div className='lg:flex-grow lg:w-1/2 flex flex-col items-center mb-12 xl:pr-20 pr-5'>
-                        <h1 className='text-white text-center lg:text-left text-3xl lg:text-[3.4rem] leading-normal mb-5'>
+                    <div className='splash-stage lg:flex-grow lg:w-1/2 flex flex-col items-center mb-12 xl:pr-20 pr-5'>
+                        <h1 className='splash-title text-white text-center lg:text-left text-3xl lg:text-[3.4rem] leading-normal mb-5'>
                             {entry.title}
                         </h1>
-                        <p className='text-center lg:text-left text-white mb-8'>
+                        <p className='splash-description text-center lg:text-left text-white mb-8'>
                             {entry.description}
                         </p>
-                        <div className='mx-auto flex flex-col md:flex-row justify-center md:justify-evenly lg:justify-start w-1/2 lg:w-full'>
+                        <div className='splash-buttons mx-auto flex flex-col md:flex-row justify-center md:justify-evenly lg:justify-start w-1/2 lg:w-full'>
                             {buttons}
                         </div>
                     </div>
                     {entry.image?.fields.file.url && 
-                        <div className='lg:max-w-lg lg:w-1/2'>
+                        <div className='splash-image lg:max-w-lg lg:w-1/2'>
                             <ContentfulImage 
                                 src={entry.image.fields.file.url} 
                                 width={550}
