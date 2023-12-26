18a849fc59183c971e7b56162564db7dae7e96bd
Nate King <nking@xtivia.com>
2023-12-25 21:13:49 -0500

    Add font imports and configurations

---
+++ lib/fonts/fonts.ts
@@ -0,0 +1,9 @@
+import { Catamaran, Dosis } from 'next/font/google';
+
+export const catamaran = Catamaran({
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+export const dosis = Dosis({
+	weight: ['200', '300', '400', '500', '600', '700'],
+	display: 'swap',
+});
